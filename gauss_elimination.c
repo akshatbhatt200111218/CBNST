67e1a197d403e6be5c60c1ab809a289599c06641
Akshat Bhatt <100102254+akshatbhattt@users.noreply.github.com>
2023-03-09 21:00:52 +0530

    Update and rename gauss elimination.c to gauss_elimination.c

--- gauss_elimination.c
+++ gauss_elimination.c
@@ -5,7 +5,7 @@ int main()
     float A[20][20],c,x[10],sum=0.0;
     printf("\nEnter the order of matrix: ");
     scanf("%d",&n);
-    printf("\nEnter the elements of augmented matrix row-wise:\n\n");
+    printf("\nEnter the elements of matrix row-wise:\n\n");
     for(i=1; i<=n; i++)
     {
         for(j=1; j<=(n+1); j++)
